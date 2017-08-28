cbe507923f14a41e43b577e6d2e170f21c2d1210
Chauvin <chauvin-e@orange.fr>
2017-08-28 14:27:37 +0200

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
-// import ReactDOM from 'react-dom';
 import React, {Component} from 'react';
+import { v4 } from 'node-uuid';
 import AddTask from './AddTask';
-
+import List from './List';
 
 class App extends Component {
 	constructor(){
@@ -10,26 +10,39 @@ class App extends Component {
 			tasks: [],
 		}
 		this._addTaskOnSubmit = this._addTaskOnSubmit.bind(this);
+		this._changeTaskStatus = this._changeTaskStatus.bind(this);
 	}
 	_addTaskOnSubmit(task){
-		this.state({
-			task : [
+		this.setState({
+			tasks : [
 
-				...
-				tasks
-
+				...this.state.tasks,
+				{content : task, id : v4(), status : false}
 			]
 		});
 
 	}
+	_changeTaskStatus(id){
+		let update = this.state.tasks;
+		update.map(item => {
+			if(item.id === id) {
+				return{content : item.content, id : item.id, status : !item.status}
+			}
+				return item;
+		});
+		this.setState({
+			tasks : update
+		});
+	}
 
-	render() {
+	render(){
 		console.log(this.state.tasks);
 		return (
 			<div>
 				<AddTask AddTask={this._addTaskOnSubmit} />
+				<List tasks={this.state.tasks} changeStatus={this._changeTaskStatus} />
 			</div>
-			)
+		)
 	}
 };
 
